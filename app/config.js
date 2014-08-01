9a0a407b9e518bb4f2ef9d4565896e6753fc854c
marko asplund <marko.asplund@gmail.com>
2014-08-01 13:34:42 +0300

    refactor configuration

---
+++ app/config.js
@@ -0,0 +1,80 @@
+
+function Config() {
+	var parseOptions = {
+		default: {
+			columnRange: [1, 7]
+		},
+		type3: {
+			columnRange: [1, 6]
+		},
+		type5: {
+			columnRange: [1, 6]
+		},
+		type6: {
+			columnRange: [1, 5]
+		}
+	};
+
+	var chartOptions = {
+    default: {
+      subtitle: {
+        text: 'TFB',
+      },
+      xAxis: {
+      	title: {
+      		text: "concurrency level"
+      	},
+        categories: ['8', '16', '32', '64', '128', '256']
+      },
+      yAxis: {
+        title: {
+          text: 'responses / second'
+        },
+        plotLines: [{
+          value: 0,
+          width: 1,
+          color: '#808080'
+        }]
+      },
+      tooltip: {
+        valueSuffix: 'responses / second'
+      },
+      legend: {
+      },
+      series: [{}]
+    },
+    type3: {
+    	xAxis: {
+    		title: {
+    			text: 'queries / request'
+    		},
+    		categories: ['1', '5', '10', '15', '20']
+    	}
+    },
+    type5: {
+    	xAxis: {
+    		title: {
+    			text: 'queries / request'
+    		},
+    		categories: ['1', '5', '10', '15', '20']
+    	}
+    },
+    type6: {
+    	xAxis: {
+    		categories: ['256', '1,024', '4,096', '16,384']
+    	}
+    },
+	};
+
+	return {
+		getDataParseOptions: function(testClass) {
+			return $.extend({}, parseOptions.default, parseOptions[testClass]);
+		},
+		getChartOptions: function(testClass, options) {
+			return $.extend({}, chartOptions.default, chartOptions[testClass], options);
+		}
+
+	}
+
+};
+
--- app/tfb.js
+++ app/tfb.js
@@ -1,100 +1,33 @@
 
-// TODO: fix scope
+function TfbData(config) {
+	this.config = config;
 
-function parseData(testClass, data) {
-	var raw = Papa.parse(data, { dynamicTyping: true });
-	var res = [];
-	var range = $.extend({}, getDataParseOptions().default, getDataParseOptions()[testClass]).columnRange;
-	for(i = 1; i < raw.data.length; i++) {
-		var a = raw.data[i];
-		if(isNaN(a[1]) || a[1] == 0) {
-			console.log("invalid data, skipping row "+i+": "+a[0]);
-			continue;
-		}
-		res.push({
-			name: a[0],
-			data: a.slice(range[0], range[1]),
-			visible: false
-		});
-	}
-	return res;
-};
-
-function getDataParseOptions() {
-	return {
-			default: {
-				columnRange: [1, 7]
-			},
-			type3: {
-				columnRange: [1, 6]
-			},
-			type5: {
-				columnRange: [1, 6]
-			},
-			type6: {
-				columnRange: [1, 5]
-			}
-	}
-};
-
-function TfbData() {
 	return {
 		getResults: function(tfbRound, testClass, onReady) {
+
+			function parseData(testClass, data) {
+				var raw = Papa.parse(data, { dynamicTyping: true });
+				var res = [];
+				var range = config.getDataParseOptions(testClass).columnRange;
+				for(i = 1; i < raw.data.length; i++) {
+					var a = raw.data[i];
+					if(isNaN(a[1]) || a[1] == 0) {
+						console.log("invalid data, skipping row "+i+": "+a[0]);
+						continue;
+					}
+					res.push({
+						name: a[0],
+						data: a.slice(range[0], range[1]),
+						visible: false
+					});
+				}
+				return res;
+			};
+
 			console.log('getResults: '+tfbRound+', '+testClass);
 			$.get('/data/'+tfbRound+'/'+'tfb-'+testClass+'.csv', function(data) {
 				onReady(parseData(testClass, data));
 			});
-		},
-		chartOptions: {
-	    default: {
-	      subtitle: {
-	        text: 'TFB',
-	      },
-	      xAxis: {
-	      	title: {
-	      		text: "concurrency level"
-	      	},
-	        categories: ['8', '16', '32', '64', '128', '256']
-	      },
-	      yAxis: {
-	        title: {
-	          text: 'responses / second'
-	        },
-	        plotLines: [{
-	          value: 0,
-	          width: 1,
-	          color: '#808080'
-	        }]
-	      },
-	      tooltip: {
-	        valueSuffix: 'responses / second'
-	      },
-	      legend: {
-	      },
-	      series: [{}]
-	    },
-	    type3: {
-	    	xAxis: {
-	    		title: {
-	    			text: 'queries / request'
-	    		},
-	    		categories: ['1', '5', '10', '15', '20']
-	    	}
-	    },
-	    type5: {
-	    	xAxis: {
-	    		title: {
-	    			text: 'queries / request'
-	    		},
-	    		categories: ['1', '5', '10', '15', '20']
-	    	}
-	    },
-	    type6: {
-	    	xAxis: {
-	    		categories: ['256', '1,024', '4,096', '16,384']
-	    	}
-	    },
-
 		},
 	}
 }
