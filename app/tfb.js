8cc66e24ab57e993ddafe3e55de5b0d2b2fd9a64
marko asplund <marko.asplund@gmail.com>
2015-04-07 22:56:39 +0300

    fix R10 results calculation; switch to using JSON result data format also for R9

--- app/tfb.js
+++ app/tfb.js
@@ -2,44 +2,30 @@
 function TfbData(config) {
 	this.config = config;
 
+	testTypes = {
+		"type1": "json",
+		"type2": "db",
+		"type3": "query",
+		"type4": "fortune",
+		"type5": "update",
+		"type6": "plaintext"
+	}
+
 	return {
 		getResults: function(tfbRound, testClass, onReady) {
-
-			function parseDataRound9(testClass, data) {
-				var raw = Papa.parse(data, { dynamicTyping: true });
-				var res = [];
-				var range = config.getDataParseOptions(testClass).columnRange;
-				for(i = 1; i < raw.data.length; i++) {
-					var a = raw.data[i];
-					if(isNaN(a[1]) || a[1] == 0) {
-						console.log("invalid data, skipping row "+i+": "+a[0]);
-						continue;
-					}
-					res.push({
-						name: a[0],
-						data: a.slice(range[0], range[1]),
-						visible: false
-					});
-				}
-				return res;
-			};
-
 			function parseData(testClass, data) {
-				var types = {
-					"type1": "json",
-					"type2": "db",
-					"type3": "query",
-					"type4": "fortune",
-					"type5": "update",
-					"type6": "plaintext"
-				}
-				var classKey = types[testClass];
+				var classKey = testTypes[testClass];
 				var res = [];
+				var duration = data['duration'];
 				var testData = data['rawData'][classKey];
 				for (var fw in testData) {
 					var counts = [];
 					for(i = 0; i < testData[fw].length; i++) {
-						counts.push(testData[fw][i]["totalRequests"]);
+						var requests = testData[fw][i]["totalRequests"];
+						var t = typeof testData[fw][i]['startTime'] == 'number' ?
+							testData[fw][i]['endTime'] - testData[fw][i]['startTime'] : duration;
+						var errors = typeof testData[fw][i]["5xx"] == 'number' ? testData[fw][i]["5xx"] : 0;
+						counts.push(Math.round( (requests-errors) / t));
 					}
 					res.push({
 						name: fw,
@@ -47,19 +33,13 @@ function TfbData(config) {
 						visible: false
 					});
 				}
-				return res;
+				return res.sort(function(a, b){return a.name.localeCompare(b.name)});
 			};
 
 			console.log('getResults: '+tfbRound+', '+testClass);
-			if(tfbRound == 'round-9') {
-				$.get('/data/round-9/tfb-'+testClass+'.csv', function(data) {
-					onReady(parseDataRound9(testClass, data));
-				});
-			} else {
-				$.get('/data/'+tfbRound+'/results.json', function(results) {
-					onReady(parseData(testClass, results));
-				});
-			}
+			$.get('/data/'+tfbRound+'/results.json', function(results) {
+				onReady(parseData(testClass, results));
+			});
 		},
 	}
 }
