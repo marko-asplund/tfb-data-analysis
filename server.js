656aab52bbccb0d7cc0985223a7ac4568e96cb15
marko asplund <marko.asplund@gmail.com>
2015-05-09 10:05:30 +0300

    set cache max-age header

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 
 var express = require('express');
+var ms = require('ms');
 var app = express();
 var mime = express.static.mime;
 
 app.use(express.static(__dirname + '/app', {
+  maxAge: ms('30m'),
   setHeaders: function(res, path, stat) {
     var c = path.split('.');
     if(c.length > 1 && c[c.length-1] == 'gz') {
