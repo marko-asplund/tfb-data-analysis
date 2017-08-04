ad52e19052106ee67bcf7b99497573958915f3fd
marko asplund <marko.asplund@gmail.com>
2017-08-04 21:24:58 +0300

    revert server.js - OpenShift cartridge runs Node.js 0.10

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 
-const express = require('express');
-const ms = require('ms');
-const app = express();
-const mime = express.static.mime;
+var express = require('express');
+var ms = require('ms');
+var app = express();
+var mime = express.static.mime;
 
 app.use(express.static(__dirname + '/app', {
   maxAge: ms('30m'),
-  setHeaders: (res, path, stat) => {
-    const c = path.split('.');
+  setHeaders: function(res, path, stat) {
+    var c = path.split('.');
     if (c.length > 1 && c[c.length-1] == 'gz') {
       res.setHeader('Content-Type', mime.lookup(c[c.length-2]));
       res.setHeader('Content-Encoding', 'gzip');
